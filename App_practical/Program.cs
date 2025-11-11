5b57874a0e995bb3803685abf1981c1a5dc187d5
Nintorch <92302738+Nintorch@users.noreply.github.com>
2025-11-11 13:50:21 +0500

    Add docker compose

--- App_practical/Program.cs
+++ App_practical/Program.cs
@@ -5,17 +5,46 @@ namespace App_practical
 {
     public class Program
     {
-        public static void Main(string[] args)
+        private static async Task WaitForDatabase(DatabaseContext context)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (await context.Database.CanConnectAsync())
+                {
+                    Console.WriteLine("Database connected!");
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine($"Waiting for database... ({i + 1}/10)");
+                    await Task.Delay(3000);
+                }
+            }
+        }
+
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<DatabaseContext>(
-                o => o.UseNpgsql("Host=postgredb;Port=5432;Database=Calculator;Username=postgres;Password=admin")
+                o => o.UseNpgsql("Host=postgres;Port=5432;Database=Calculator;Username=postgres;Password=admin")
                 );
 
             var app = builder.Build();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var context = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+
+                // ���� ��
+                await WaitForDatabase(context);
+
+                // ������������� ������� ������� �� ������ �������
+                await context.Database.EnsureCreatedAsync();
+                Console.WriteLine("Database tables created!");
+            }
 
             // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
